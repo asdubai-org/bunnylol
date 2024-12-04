2d9ddacee0e7a034f14a3c1cad11b57884bee6d9
Ken Simonds <ksimonds@asdubai.org>
2024-12-04 13:21:39 +0400

    add nyt

--- src/Commands.js
+++ src/Commands.js
@@ -14,11 +14,6 @@ const COMMANDS = {
         url: "https://lookerstudio.google.com/",
         searchurl: "https://lookerstudio.google.com/navigation/reporting?search="
     },
-    fb: {
-        name: "Facebook",
-        url: "https://facebook.com/",
-        searchurl: "https://www.facebook.com/search/top/?q="
-    },
     wa: {
         name: "WhatsApp Web",
         url: "https://web.whatsapp.com/"
@@ -38,29 +33,9 @@ const COMMANDS = {
         url: "https://www.google.com/maps/",
         searchurl: "https://www.google.com/maps/search/"
     },
-    yt: {
-        name: "YouTube",
-        url: "https://youtube.com/",
-        searchurl: "https://www.youtube.com/results?search_query="
-    },
-    gh: {
-        name: "GitHub",
-        url: "https://github.com/",
-        searchurl: "https://www.github.com/search?q="
-    },
     l: {
         name: "Linkedin",
         url: "https://linkedin.com/",
-    },
-    ig: {
-        name: "Instagram",
-        url: "https://instagram.com/",
-        searchurl: "https://instagram.com/"
-    },
-    n: {
-        name: "Netflix",
-        url: "https://netflix.com/",
-        searchurl: "https://www.netflix.com/search?q="
     },
     cal: {
         name: "Google Calendar",
@@ -91,6 +66,11 @@ const COMMANDS = {
         name: "Airflow",
         url: "https://airflow.asdubai.org/",
         searchurl: "https://airflow.asdubai.org/dagrun/list/?_flt_3_state="
+    },
+    nyt: {
+        name: "New York Times",
+        url: "https://www.nytimes.com/",
+        searchurl: "https://www.nytimes.com/search?query="
     },
     DEFAULT: {
         name: "Default - Google Search",
